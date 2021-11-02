9779ee0988be8054d07c3ee3ba3f586f97dcfb4d
Andrew Newlands <andrewn@remasys.com>
2021-11-02 13:46:33 +1100

    Remove grid entries upon removing configured library

--- gamekeeper/MainWindow.xaml.cs
+++ gamekeeper/MainWindow.xaml.cs
@@ -5,6 +5,8 @@ using System.Windows;
 using System.Windows.Controls;
 using System.IO;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
 
 namespace gamekeeper
 {
@@ -24,10 +26,35 @@ namespace gamekeeper
         public MainWindow()
         {
             _model = new Model();
+            this._model.Configuration.libraries.CollectionChanged += OnLibraryChanged;
             DataContext = _model;
             InitializeComponent();
 
         }
+
+        private void OnLibraryChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                for (var i = this._model.Games.Count - 1; i >= 0; i--)
+                {
+                    foreach (Library lib in e.OldItems)
+                    {
+                        if (this._model.Games[i].Library == lib.name)
+                        {
+                            this._model.Games.RemoveAt(i);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                MessageBox.Show("Add not implemented");
+            }
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var sw = new Settings(ref this._model);
